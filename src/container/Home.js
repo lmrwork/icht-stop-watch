33312740f5d2ec34b0d310659dd1919f85d94a1a
LMR <59361885@qq.com>
2017-08-24 19:47:50 +0800

    考试2

--- src/container/Home.js
+++ src/container/Home.js
@@ -26,7 +26,7 @@ export class Home extends PureComponent {
         <NavBar rightContent={ <Icon onClick={ this.onSetting } key="1" type="ellipsis" /> } leftContent={ 'home' } onLeftClick={ () => alert('然而并没有首页！') }> 
           { this.props.lang.homeTitle } 
         </NavBar>
-        <Watch deg={60}/>
+        <Watch start={5}/>
         <WingBlank>
           <Button className="btn" onClick={ this.onStart }> 开始计时 </Button>
         </WingBlank>

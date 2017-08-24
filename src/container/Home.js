8fdf8151b3064de4c06cd55096f3c522a52a3166
LMR <59361885@qq.com>
2017-08-24 20:22:42 +0800

    考试：添加reset按钮

--- src/container/Home.js
+++ src/container/Home.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
-import { NavBar, Icon, Button, WingBlank } from 'antd-mobile';
+import { NavBar, Icon, Button, WingBlank, WhiteSpace } from 'antd-mobile';
 //action
 import { goSetting } from '../action/react-stop-watch';
 //component
@@ -9,15 +9,43 @@ import { Watch } from '../component/Watch';
 export class Home extends PureComponent {
   constructor(props) {
      super(props);
-     this.state = {};
+     this.state = {
+        second: 0,
+        timer: null,
+        disableStart: false,
+        disableStop: true,
+        disableReset: true,
+     };
    }
 
   onSetting = () => {
     this.props.goSetting(this.props.history);
   }
 
-  onStart = () => {
-    alert('start');
+  start = () => {
+    //开始计时
+    const timer = setInterval(() => {
+      this.setState((prevState) => {
+        return {second: prevState.second + 0.1};
+      });
+    }, 100);
+    //保存计时状态
+    this.setState({
+      timer: timer,
+      disableStart: true,
+      disableStop: false,
+    });
+  }
+
+  stop = () => {
+    if (this.state.timer) {
+      clearInterval(this.state.timer);
+      this.setState({
+        timer: null,
+        disableStart: false,
+        disableStop: true,
+      });
+    }
   }
 
   render() {
@@ -26,9 +54,11 @@ export class Home extends PureComponent {
         <NavBar rightContent={ <Icon onClick={ this.onSetting } key="1" type="ellipsis" /> } leftContent={ 'home' } onLeftClick={ () => alert('然而并没有首页！') }> 
           { this.props.lang.homeTitle } 
         </NavBar>
-        <Watch second={5}/>
+        <Watch second={this.state.second} />
         <WingBlank>
-          <Button className="btn" onClick={ this.onStart }> 开始计时 </Button>
+          <Button className="btn" disabled={this.state.disableStart} onClick={ this.start }> 开始计时 </Button>
+          <WhiteSpace />
+          <Button className="btn" disabled={this.state.disableStop} onClick={ this.stop } type="warning"> 暂停计时 </Button>
         </WingBlank>
       </div>
     );
